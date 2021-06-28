c24968ce27d027d3133e583c00db546ab21c9d6b
Sebastian Paarmann <sebastian@s-paarmann.de>
2021-06-28 10:15:29 +0200

    Project Structure

--- Editor/TimeSlider.cs
+++ Editor/TimeSlider.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class TimeSlider : EditorWindow {
+namespace UnityUtils {
+	public class TimeSlider : EditorWindow {
+		[MenuItem("Window/Util/Time Slider")]
+		public static void OpenWindow() => GetWindow<TimeSlider>(false, "Time", true);
 
-	[MenuItem("Window/Util/Time Slider")]
-	public static void OpenWindow() => GetWindow<TimeSlider>(false, "Time", true);
-
-	private void OnGUI() {
-		if (Application.isPlaying) {
-			GUILayout.Label($"Time Scale: {Time.timeScale}");
-			Time.timeScale = GUILayout.HorizontalSlider(Time.timeScale, 0f, 2f);
-		}
-		else {
-			GUILayout.Label("Use Project Settings to modify time scale in edit mode!");
+		private void OnGUI() {
+			if (Application.isPlaying) {
+				GUILayout.Label($"Time Scale: {Time.timeScale}");
+				Time.timeScale = GUILayout.HorizontalSlider(Time.timeScale, 0f, 2f);
+			}
+			else {
+				GUILayout.Label("Use Project Settings to modify time scale in edit mode!");
+			}
 		}
 	}
 }
